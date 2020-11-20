73dbca003ea96b884ef7094a3053e21f1d5899d3
gadepall <gadepall@gmail.com>
2020-11-20 12:09:10 +0530

    Add existing files

---
+++ probability/exercises/prob/solutions/1/3/assignment4.py
@@ -0,0 +1,7 @@
+n=10
+p=5/6
+q=1-p
+px0 = pow(5,10)/pow(6,10)
+px1 = (pow(5,9)*10)/pow(6,10)
+pxlessthan2 = px0+px1
+print("Probability of hurdles fewer than 2 =",pxlessthan2)
---
+++ probability/probman/codes/binomial/binomial.py
@@ -0,0 +1,42 @@
+#Code by GVV Sharma
+#November 20,2020
+#Released under GNU/GPL
+#To find the probability of an event using the binomial distribution
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import bernoulli
+from scipy.stats import binom
+
+
+
+
+#Simlen
+simlen=1000
+
+#Number of hurdles
+n = 10
+
+#Probability of  clearing a hurdle
+p = 5/6
+
+
+#Theoretical probability of knocking down fewer than 2 hurdles
+k = 1
+print(binom.cdf(k, n, 1-p),3*(5/6)**10)
+
+
+#Simulating the probability using  the binomial random variable
+data_binom = binom.rvs(n,1-p,size=simlen) #Simulating the event of jumping 10 hurdles
+err_ind = np.nonzero(data_binom <=k) #checking probability condition
+err_n = np.size(err_ind) #computing the probability
+print(err_n/simlen)
+
+
+#Simulating the probability using  the bernoulli random variable
+data_bern_mat = bernoulli.rvs(1-p,size=(n,simlen))
+data_binom=np.sum(data_bern_mat, axis=0)
+#print(data_binom_bern)
+err_ind = np.nonzero(data_binom <=k) #checking probability condition
+err_n = np.size(err_ind) #computing the probability
+print(err_n/simlen)
